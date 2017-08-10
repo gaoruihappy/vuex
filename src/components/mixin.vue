b54e782bd3a62a3554f39d55515cc7ab79ff0471
gaorui <gaorui@lianjia.com>
2017-08-10 19:13:40 +0800

    toast

---
+++ src/components/mixin.vue
@@ -0,0 +1,31 @@
+
+// 定义一个混合对象
+<template>
+<div>mixin{{msg}}</div>
+</template>
+<script>
+import Vue from 'vue'
+export default {
+  name: 'mymixin',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+var myMixin = {
+  created: function () {
+    this.hello()
+  },
+  methods: {
+    hello: function () {
+      console.log('hello from mixin!')
+    }
+  }
+}
+// 定义一个使用混合对象的组件
+var Component = Vue.extend({
+  mixins: [myMixin]
+})
+var component = new Component() // -> "hello from mixin!"
+</script>
--- src/main.js
+++ src/main.js
@@ -5,8 +5,31 @@ import App from './App'
 import router from './router'
 import Vuex from 'vuex';
 import store from './store/store'
+import Toast from './components/toast/index'
 Vue.config.productionTip = false
 Vue.use(Vuex)
+Vue.use(Toast)
+Vue.directive('focus', {
+  // 当绑定元素插入到 DOM 中。
+  inserted: function (el) {
+    // 聚焦元素
+    el.focus()
+  }
+})
+Vue.directive('demo', {
+  update: function (el, binding, vnode) {
+  	// debugger
+    var s = JSON.stringify
+    el.innerHTML =
+      'name: '       + s(binding.name) + '<br>' +
+      'value: '      + s(binding.value) + '<br>' +
+      'oldValue: '      + s(binding.oldValue) + '<br>' +
+      'expression: ' + s(binding.expression) + '<br>' +
+      'argument: '   + s(binding.arg) + '<br>' +
+      'modifiers: '  + s(binding.modifiers) + '<br>' +
+      'vnode keys: ' + Object.keys(vnode).join(', ')
+  }
+})
 
 
 /* eslint-disable no-new */
